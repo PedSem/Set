07d78d3925c0aece63435676995de767863e5cba
PedSem <153596408+PedSem@users.noreply.github.com>
2024-03-26 20:52:16 +0100

    Create PlanetaEnano.java

---
+++ PlanetaEnano.java
@@ -0,0 +1,7 @@
+public class PlanetaEnano extends CuerpoCeleste{
+    public PlanetaEnano(String nombre, double periodoOrbital) {
+        super(nombre, periodoOrbital,TipoCuerpoCeleste.planeta_enano);
+
+
+    }
+}
